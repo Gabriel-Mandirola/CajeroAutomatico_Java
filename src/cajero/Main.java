96216d017e04b42a72ca94b228af02b58d0a8387
Gabriel Mandirola <Gabriel_Mandirola@hotmail.com>
2019-10-10 12:32:20 -0300

    Este es un projecto sobre un cajero automatico usando el paradiga de la programacion orientada a objetos ( encapsulamiento, metodos setter y getter, herencia, polimorfismo y ámbito de variables)

---
+++ src/cajero/Main.java
@@ -0,0 +1,14 @@
+package cajero;
+
+import plata.MenuOpciones;
+
+
+public class Main {
+    public static void main(String args[]){
+        
+        
+        MenuOpciones mensajero = new MenuOpciones();
+        mensajero.opciones();
+        
+    }
+}
